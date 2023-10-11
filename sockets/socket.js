0d100d808842dd723868be699abe76f27e76d449
Adrian González <adrianalexisgonzalezherrera@gmail.com>
2023-10-11 23:10:36 +0200

    Fin Sección 10 - Backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,47 @@
+//const { Client } = require('socket.io/dist/client');
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket');
+// const { Client } = require('socket.io/dist/client');
+
 //   Mensajes De Sockets
 io.on('connection', client => {
     // client.on('event', data => { /* … */ });
     // client.on('disconnect', () => { /* … */ });
     console.log('Cliente Conectado');
 
+    // console.log( client.handshake.headers[ 'x-token' ] );
+    const [ valido, uid ] = comprobarJWT( client.handshake.headers[ 'x-token' ] );
+
+    //* Verificar Autenticación
+    // console.log( valido, uid );
+    if ( !valido ) { return client.disconnect(); }
+
+    //* Cliente Autenticado
+    usuarioConectado( uid );
+
+    //* Ingresar Al Usuario A una Sala En Particular
+    // Sala Global, Client.id, 650c04b46e56b8365b06fb89
+    
+    client.join( uid );
+
+    //* Escuchar Del Cliente El  Mensaje-Personal
+    
+    client.on( 'mensaje-personal', async ( payload ) => {
+        // console.log( payload );
+        await grabarMensaje( payload );
+        io.to( payload.para ).emit( 'mensaje-personal', payload );
+    } );
+    
+
+
+    
+    // console.log('Cliente Autenticado')
+
+
     client.on('disconnect', () => { 
-        console.log('Cliente Desconectado')
+        console.log('Cliente Desconectado');
+        usuarioDesconectado( uid );
     });
 
     //client.on('mensaje', ( payload ) => {
